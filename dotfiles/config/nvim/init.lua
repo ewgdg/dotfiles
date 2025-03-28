f4a75efaac52ff292ff684a647bf8713d47028db
Xian Zhang <xianzzz@outlook.com>
2025-03-27 17:47:51 -0700

    update vim line number

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -5,6 +5,8 @@ vim.opt.expandtab = true -- Use spaces instead of tabs
 vim.opt.autoindent = true -- Copy indentation from the previous line
 vim.opt.smartindent = true -- Smart auto-indentation
 
+vim.wo.number = true
+
 -- vim.opt.clipboard = "unnamedplus"
 
 -- lazy.nvim plugin manager
