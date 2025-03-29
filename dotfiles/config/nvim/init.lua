740b561cef5fa16de86c170be415b840e49217b8
Xian Zhang <xianzzz@outlook.com>
2025-03-29 16:54:59 -0700

    update nvim

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -2,8 +2,9 @@
 vim.opt.tabstop = 4      -- Number of spaces that a <Tab> counts for
 vim.opt.shiftwidth = 4   -- Number of spaces to use for indentation
 vim.opt.expandtab = true -- Use spaces instead of tabs
-vim.opt.autoindent = true -- Copy indentation from the previous line
-vim.opt.smartindent = true -- Smart auto-indentation
+-- vim.opt.smartindent = true -- Smart auto-indentation, old logic, do not use
+-- `filetype plugin indent on` is enabled by default in nvim
+-- vim.opt.autoindent = true -- Copy indentation from the previous line, enabled by default
 
 vim.wo.number = true
 
