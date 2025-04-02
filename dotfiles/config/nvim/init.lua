1a791f06103fc4d365ad1523c34419de4af33cf4
Xian Zhang <xianzzz@outlook.com>
2025-04-01 18:12:12 -0700

    add lazyvim plugin

--- dotfiles/config/nvim/init.lua
+++ dotfiles/config/nvim/init.lua
@@ -1,15 +1,3 @@
--- Set indentation to 4 spaces
-vim.opt.tabstop = 4      -- Number of spaces that a <Tab> counts for
-vim.opt.shiftwidth = 4   -- Number of spaces to use for indentation
-vim.opt.expandtab = true -- Use spaces instead of tabs
--- vim.opt.smartindent = true -- Smart auto-indentation, old logic, do not use
--- `filetype plugin indent on` is enabled by default in nvim
--- vim.opt.autoindent = true -- Copy indentation from the previous line, enabled by default
-
-vim.wo.number = true
-
--- vim.opt.clipboard = "unnamedplus"
-
 -- lazy.nvim plugin manager
 require("config.lazy")
 
