4527d7fb4ec2a319094351a0339482e40469616d
Xian Zhang <xianzzz@outlook.com>
2025-04-07 00:56:24 -0700

    fix xml format

--- scripts/xmlformat.py
+++ scripts/xmlformat.py
@@ -13,25 +13,21 @@ def merge_nodes(
         cur_path: str,
         paths_to_merge: list[str] | None = None,
     ):
-        layer1_tag_map = {child.tag: (child, i) for i, child in enumerate(layer1)}
+        layer1_child_map = {child.tag: (child, i) for i, child in enumerate(layer1)}
         for layer2_child in layer2:
             child_path = f"{cur_path}/{layer2_child.tag}"
+            should_merge = False
             if paths_to_merge:
                 for path_to_merge in paths_to_merge:
                     if fnmatch.fnmatch(child_path, path_to_merge):
-                        print(f"Merging element: {child_path}")
-                        if layer2_child.tag in layer1_tag_map:
-                            layer1_child, layer1_child_i = layer1_tag_map[
-                                layer2_child.tag
-                            ]
-                            layer1[layer1_child_i] = layer2_child
-                        else:
-                            layer1.append(layer2_child)
-                        break
-            elif layer2_child.tag in layer1_tag_map:
-                layer1_child, layer1_child_i = layer1_tag_map[layer2_child.tag]
-                if len(layer2_child) == 0:
-                    print(f"Merging element: {child_path}")
+                        should_merge = True
+            elif len(layer2_child) == 0 or layer2_child.tag not in layer1_child_map:
+                should_merge = True
+
+            if layer2_child.tag in layer1_child_map:
+                layer1_child, layer1_child_i = layer1_child_map[layer2_child.tag]
+                if should_merge:
+                    print(f"Merging element {child_path} to index {layer1_child_i}")
                     layer1[layer1_child_i] = layer2_child
                 else:
                     merge_nodes_recursion(
@@ -40,7 +36,7 @@ def merge_nodes(
                         child_path,
                         paths_to_merge,
                     )
-            else:
+            elif should_merge:
                 # Add new child node if not present
                 print(f"Adding new element: {child_path}")
                 layer1.append(layer2_child)
@@ -55,13 +51,18 @@ def remove_nodes(cur: ET.Element, paths_to_remove: list):
         cur_path: str,
         paths_to_remove: list[str],
     ):
+        # print(f"Current, {cur.tag},  path: {cur_path}")
         for path_to_remove in paths_to_remove:
             # Check if the current path matches the pattern to ignore
-            if parent is not None and fnmatch.fnmatch(cur_path, path_to_remove):
+            if fnmatch.fnmatch(cur_path, path_to_remove):
                 print(f"Removing element: {cur_path}")
-                parent.remove(cur)
+                if parent is not None:
+                    parent.remove(cur)
+                else:
+                    cur.clear()
+                return
 
-        for child in cur:
+        for child in list(cur):  # make a copy of children to avoid runtime error
             child_path = f"{cur_path}/{child.tag}"
             # Recursively check child elements
             remove_nodes_recursion(child, cur, child_path, paths_to_remove)
