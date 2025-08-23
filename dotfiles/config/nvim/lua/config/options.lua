44ec5bba07bcdcb2d981241378074eb511181298
Xian Zhang <xianzzz@outlook.com>
2025-08-22 18:15:37 -0700

    Update options.lua

--- dotfiles/config/nvim/lua/config/options.lua
+++ dotfiles/config/nvim/lua/config/options.lua
@@ -4,3 +4,15 @@
 -- Add any additional options here
 
 vim.opt.wrap = true
+
+-- Configure clipboard to only sync yanks, not deletions
+vim.opt.clipboard = ""  -- Disable automatic clipboard sync
+-- Create autocmd to sync only yanks to system clipboard
+vim.api.nvim_create_autocmd("TextYankPost", {
+  pattern = "*",
+  callback = function()
+    if vim.v.event.operator == "y" then
+      vim.fn.setreg("+", vim.fn.getreg('"'))
+    end
+  end,
+})
