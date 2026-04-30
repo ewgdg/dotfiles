0fefbd731c359297a3ab3522566aba5dde1f46be
Xian Zhang <xianzzz@outlook.com>
2026-04-30 16:36:13 -0700

    add go path

--- packages/shell/files/env.core.sh
+++ packages/shell/files/env.core.sh
@@ -20,6 +20,15 @@ export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
 export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
 export BUN_INSTALL="${BUN_INSTALL:-$HOME/.bun}"
 
+go_path="${GOPATH:-}"
+if [ -z "$go_path" ] && command -v go >/dev/null 2>&1; then
+  go_path="$(go env GOPATH 2>/dev/null)"
+fi
+if [ -n "$go_path" ]; then
+  path_prepend "$go_path/bin"
+fi
+unset go_path
+
 path_prepend "$BUN_INSTALL/bin"
 path_prepend "$HOME/.npm/bin"
 path_prepend "$HOME/.cargo/bin"
